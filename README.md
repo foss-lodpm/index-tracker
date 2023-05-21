6633e2402555ba353f37249f572f7c36a62e0c27
ozkanonur <work@onurozkan.dev>
2023-05-21 15:51:03 +0300

    initialize index-tracker API
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # index-tracker
-Retrieve list of SQL patches corresponding to a specified timestamp for LPM package indexes
---
+++ api.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"compress/gzip"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var PATCH_DIR string
+
+func getPatch(timestamp uint64) (string, error) {
+	query := fmt.Sprintf("cat $(ls -1 | awk '$0 >= %d')", timestamp)
+
+	cmd := exec.Command("sh", "-c", query)
+	cmd.Dir = PATCH_DIR
+
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	return string(output), nil
+}
+
+func extractTimestampArg(path string) (uint64, error, int) {
+	parts := strings.Split(path, "/")
+
+	arg := parts[1]
+	if len(parts) != 2 || len(arg) == 0 {
+		return 0, errors.New("Not Found"), http.StatusNotFound
+	}
+
+	timestamp, err := strconv.ParseUint(arg, 10, 64)
+
+	if err != nil {
+		return 0, errors.New(fmt.Sprintf("%s is an invalid argument, expected a UNIX timestamp", arg)), http.StatusBadRequest
+	}
+
+	return timestamp, nil, 0
+}
+
+func endpointHandler(w http.ResponseWriter, r *http.Request) {
+	responseCh := make(chan string)
+
+	go func() {
+		path := r.URL.Path
+
+		w.Header().Set("Content-Type", "text/plain")
+
+		timestamp, err, httpErrCode := extractTimestampArg(path)
+		if err != nil {
+			http.Error(w, err.Error(), httpErrCode)
+			return
+		}
+
+		patch, err := getPatch(timestamp)
+		if err != nil {
+			http.Error(w, "Query was failed.", http.StatusInternalServerError)
+			return
+		}
+
+		responseCh <- patch
+	}()
+
+	select {
+	case response := <-responseCh:
+		contentLength := strconv.Itoa(len(response))
+		w.Header().Set("Content-Length", contentLength)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(response))
+	case <-time.After(5 * time.Second): // timeout handling
+		w.WriteHeader(http.StatusRequestTimeout)
+		w.Write([]byte("Timeout exceeded"))
+	}
+
+}
+
+type gzipResponseWriter struct {
+	io.Writer
+	http.ResponseWriter
+}
+
+func (grw gzipResponseWriter) Write(b []byte) (int, error) {
+	return grw.Writer.Write(b)
+}
+
+func gzipMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// only compress if client supports gzip encoding
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			w.Header().Set("Content-Encoding", "gzip")
+			gzipWriter := gzip.NewWriter(w)
+			defer gzipWriter.Close()
+
+			// replace the response writer
+			w = gzipResponseWriter{Writer: gzipWriter, ResponseWriter: w}
+		}
+
+		// move to next handler
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	PATCH_DIR = os.Getenv("PATCH_DIR")
+	if PATCH_DIR == "" {
+		log.Fatal("PATCH_DIR environment is not present.")
+	}
+
+	fmt.Printf("index-tracker server is listening on port 6150 for %s\n", PATCH_DIR)
+	log.Fatal(http.ListenAndServe(":6150", gzipMiddleware(http.HandlerFunc(endpointHandler))))
+}
